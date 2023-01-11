5cd15640b3a7ca84b83a547e0dc413cd2e7fe1d1
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-11 23:10:17 +0530

    Add files via upload

--- basic.java
+++ basic.java
@@ -1,16 +1,12 @@
-import java.util.*;
-
-public class Solution {
-
-    public static void main(String[] args) 
+import java.io.*;
+class A
+{
+    public static void main(String args[])
     {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        Console c=System.console();
+        int n1=Integer.parseInt(c.readLine("Enter First Number:"));
+        int n2=Integer.parseInt(c.readLine("Enter First Number:"));
+        int sum=n1+n2;
+        System.out.println(sum);
     }
 }
