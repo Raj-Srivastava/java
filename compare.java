681de483e30eaf36e8b4e93a2369ef8f038ff222
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-12 23:39:47 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class compare 
+{
+public static void main(String[] args) { 
+Scanner input = new Scanner(System.in);
+System.out.print("Enter a number: ");
+    int number = input.nextInt();
+    if( number == 0)
+    { System.out.println("Number is equal to zero"); }
+    else if (number > 0)
+    { System.out.println("Number is positive"); }
+    else 
+    { System.out.println("Number is negative"); }
+
+
+  }
+ }
