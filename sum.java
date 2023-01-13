dde3e2c182bca87cb5804b5b0f20de848783bd1b
Raj-Srivastava <77150093+Raj-Srivastava@users.noreply.github.com>
2023-01-13 23:44:47 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import  java.util.Scanner;
+public class sum {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Input the 5 numbers: ");
+        for(int i=1; i<=5; i++){
+            int num = input.nextInt();
+            sum += num;
+        }
+        System.out.println("The sum of the given number is " + sum);
+
+    }
+}
